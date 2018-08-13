a6fbd99bd144ff607cbeb339464594093c566f04
Christian <christian.guest@outlook.com>
2018-08-13 11:04:02 +0100

    Added jest and an inital test

---
+++ src/app/components/Pitch/__tests__/Pitch.test.ts
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import { shallow } from 'enzyme';
+import Pitch from './../'
+
+describe('Pitch', () => {
+  it('should be defined', () => {
+    expect(Pitch).toBeDefined()
+  })
+})
